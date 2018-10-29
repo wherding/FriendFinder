f0942b69171a20e617325dfd9b08911f572d1648
wherding <wherding@outlook.com>
2018-10-28 23:19:46 -0700

    messing with heroku

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var express = require('express')
 
 var app = express()
 
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
 var PORT = process.env.port || 8080; 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 require("./app/routing/htmlRoutes")(app);
 require('./app/routing/apiRoutes')(app);
 
