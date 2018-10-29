fe9c1654b481d821e63ca7d5586f08625fdfd32b
wherding <wherding@outlook.com>
2018-10-28 22:58:54 -0700

    struggling with adding js to html for survey

---
+++ app/public/survey.js
@@ -0,0 +1,35 @@
+var survey = [
+s1 ={   q: "i am a hard worker", 
+        a: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
+},
+s2 ={   q: "i drink alot", 
+        a: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
+},
+s3 ={   q: "i go to the gym everyday", 
+a: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
+},
+s4 ={   q: "money is important to me", 
+a: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
+},
+s5 ={   q: "looks are everything", 
+a: ["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"]
+},
+
+];
+
+
+
+survey.forEach(e => {
+  var q =  $("<div>")
+  q.text(e.q)
+  
+  $(".survey").append(q);
+  e.a.forEach(i => {
+      var a = $("<inuput>")
+      a.attr("type", "radio");
+      a.attr("value",i);
+      a.attr("name",e.q);
+      (".survey").append(a);
+  });
+
+});
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,15 @@ let path = require('path');
 module.exports = function (app) {
 
     app.get('/survey', function (req, res) {
+        console.log(__dirname)
         res.sendFile(path.join(__dirname, '../public/survey.html'));
+        res.sendFile(path.join(__dirname, '../public/survey.js'));
+        //app.use(express.static(path.join(__dirname, '../public/survey.js')));
     });
 
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
+        
     });
 
 };
