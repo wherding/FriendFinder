b34a573c7e3b1236b979a114528969137fb15d18
William Herding <william.herding@cableone.biz>
2018-11-02 17:23:35 -0700

    i think i'm done

--- app/data/frinds.js
+++ app/data/frinds.js
@@ -15,6 +15,10 @@ var friends = [
         1
       ]
   },
+  {"name":"Sterling Archer","photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Farcher%2Fimages%2F3%2F3f%2FSterling_Archer_Standing_POSE.png%2Frevision%2Flatest%3Fcb%3D20151123090437&imgrefurl=http%3A%2F%2Farcher.wikia.com%2Fwiki%2FSterling_Archer&docid=8qdo36zYsruFGM&tbnid=wrTPI7-gf5bMoM%3A&vet=10ahUKEwjG8Zup-bbeAhUthOAKHX1wC_gQMwhvKAAwAA..i&w=400&h=1000&bih=796&biw=1602&q=sterling%20archer&ved=0ahUKEwjG8Zup-bbeAhUthOAKHX1wC_gQMwhvKAAwAA&iact=mrc&uact=8",
+  "scores":[5,5,5,1,1,5,5,1,1,5]
+},
+
 
 
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,44 @@ module.exports = function(app){
         res.json(friends);
     });
 
-app.post("/api/friends", function(req, res) {
-   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-   // It will do this by sending out the value "true" have a table
-   // req.body is available since we're using the body parsing middleware
+    app.post('/api/friends', function (req, res) {
 
-     friends.push(req.body);
-     res.json(false);
- });
+        var newFriend = req.body;
+    
+        var bestMatch = {};
+    
+        for (var i = 0; i < newFriend.scores.length; i++) {
+          if (newFriend.scores[i] == "1 (Strongly Disagree)") {
+            newFriend.scores[i] = 1;
+          } else if (newFriend.scores[i] == "5 (Strongly Agree)") {
+            newFriend.scores[i] = 5;
+          } else {
+            newFriend.scores[i] = parseInt(newFriend.scores[i]);
+          }
+        }
+    
+        var bestMatchIndex = 0;
+    
+        var bestMatchDifference = 40;
+    
+        for (var i = 0; i < friends.length; i++) {
+          var totalDifference = 0;
+    
+          for (var index = 0; index < friends[i].scores.length; index++) {
+            var differenceOneScore = Math.abs(friends[i].scores[index] - newFriend.scores[index]);
+            totalDifference += differenceOneScore;
+          }
+    
+          if (totalDifference < bestMatchDifference) {
+            bestMatchIndex = i;
+            bestMatchDifference = totalDifference;
+          }
+        }
+    
+        bestMatch = friends[bestMatchIndex];
+    
+        friends.push(newFriend);
+    
+        res.json(bestMatch);
+      });
 };
